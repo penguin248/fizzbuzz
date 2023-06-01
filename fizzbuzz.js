5971007d9146669cfdef9e417b44ea5337a57519
James_Matara <jamesmatara11@gmail.com>
2023-06-01 10:41:20 +0300

    fizz

---
+++ fizzbuzz.js
@@ -0,0 +1,8 @@
+//FIZZBUZZ programme
+
+for (let num = 1; num <= 100; num++) {
+    let output = "";
+    if (num % 3 == 0) output += "Fizz";
+    if (num % 5 == 0) output += "Buzz";
+    console.log(output || num);
+  }
